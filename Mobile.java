bbc9ccab7fa959daf8cf6065109d2a8fb1e16a6e
nigil-7191 <115300492+nigil-7191@users.noreply.github.com>
2022-10-09 13:05:16 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+package week1day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+	Mobile M = new Mobile();
+		System.out.println(M.sendSMS());
+		System.out.println(M.takeaSnapshot());
+	}
+	
+		
+			public String sendSMS() {
+			String message = "hello";
+			return message;
+		}
+		
+					
+			public String takeaSnapshot() {
+			String click = "take a snap";
+			return click;
+		}			
+
+	}
+
