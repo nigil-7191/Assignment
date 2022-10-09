b2ee7ae19c6f6c41fd9304cb2060bdd0cbc837c2
nigil-7191 <115300492+nigil-7191@users.noreply.github.com>
2022-10-09 12:22:24 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,23 @@
+package week1day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 100.2f;
+			String name = "firefox";
+			  boolean  visible = true;
+			   int  year = 1998;
+			   char  logo = 'm' ;
+			   
+			   System.out.println("version is" + version);
+			   System.out.println (visible);
+			   System.out.println("name is" + name);
+			   System.out.println("year is" + year);
+			   System.out.println("logo is" + logo);
+			   
+
+	}
+
+}
